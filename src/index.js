e5eca51a17bd4eefe3087053126b527884f7dfc6
Zach Machtinger <zbm8121@rit.edu>
2022-10-03 23:43:43 -0400

    Created a simple Shopping List app

--- src/index.js
+++ src/index.js
@@ -5,11 +5,33 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+function Item(props) {
+  return <div className="item">
+  <b>Name:</b> {props.name} <br />
+  <b>Price:</b> ${props.price}
+  </div>;
+}
+
+function List() {
+  return <div>
+    <Item name="Cheese" price="4.99" />
+    <Item name="Bread" price="1.5" />
+    <Item name="Ice cream" price="24" />
+  </div>;
+}
+
+const el = <List />; 
+root.render(el);
+
+/* REPLACED: initial react code
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+*/
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
